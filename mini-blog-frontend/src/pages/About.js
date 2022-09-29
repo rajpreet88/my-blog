0a4c39086e9db9ff3bdc4a6d3d1c9ec7b1b35723
Preetam <sahupreetamkumar@gmail.com>
2022-09-29 16:42:41 +0530

    Create About.js

---
+++ mini-blog-frontend/src/pages/About.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
+        Navratri Festival
+      </h1>
+      <br />
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        <b>'Navratri'</b> means <b>'nine nights</b>.' 'Nava' means 'nine,’ and 'Ratri' means
+        'night.' Night provides rest and rejuvenation. During the night, you
+        turn inwards through sleep, and you wake up feeling refreshed and rested
+        in the morning. In the same way, Navratri or the ‘nine nights’ is that
+        time of the year when you get the chance to experience deep rest. This
+        deep rest brings freedom from all kinds of botherations, deep
+        relaxation, and creativity. Fasting, meditation, prayers, and other
+        spiritual practices performed during this period help bring about this
+        deep rest. Even refraining from over-indulging in sense objects during
+        this time aid the process of attaining deep rest. Our spirit: The source
+        of deep rest Our spirit has existed since time immemorial. It is the
+        boundless and the eternal source of energy of this universe. During
+        Navratri, even the subtle energies in the environment enhance and assist
+        one’s experience of reaching the spirit. The prayer, chanting, and
+        meditation performed during Navratri connect us with our spirit. Getting
+        in touch with the spirit invokes positive qualities within us and
+        destroys laziness, pride, obsession, cravings, and aversions. When
+        stress in the form of negative emotions is destroyed, we experience the
+        deep rest of the transforming nine nights. Devi Puja: Honoring the
+        Omnipresent Energy Devi represents the omnipresent cosmic Energy. The
+        whole Creation is permeated by this Energy. The prosperity that we enjoy
+        in our daily lives is a manifestation of Devi. Mother Divine serves us
+        in so many forms. The forms of our mother, father, friends, husband,
+        wife, son, daughter and also the Guru. The revolving planets and the
+        moon is Devi performing aarti to us. Through puja we say, “Oh Mother,
+        whatever you give to me I give back to you. For example, during the
+        Puja, we offer food grains to Devi as Nature provides us with food. Devi
+        Puja is an expression of expanded consciousness showing reverence to the
+        whole Creation. The puja that we perform during the 9 days of Navratri
+        is a way of honouring the Devi and showing our gratitude to the Mother
+        Divine. While attending the puja we give up all our worldly activities
+        for a while and enter into deep meditation. Gurudev Sri Sri Ravi Shankar
+        says, “There is Devi energy (Shakti) within all of us. Devi is not
+        somewhere else, not in some other world. If we sit deep in meditation,
+        the glow within the body will brighten, and will expand and spread
+        outside. This is Devi Puja.”
+        <br/><br/>
+        <b className="text-2xl">Nine colors of Navratri 2022</b>
+        The nine colors of Navratri 2022 are white, red, royal blue, yellow,
+        green, grey, orange, peacock green, and pink. Every year, while the
+        colors remain the same, the order varies depending on the day Navratri
+        falls. Here is a list of the Navratri colors 2022:
+    
+          <li className="text-gray-900">First Day (26th September) - Pratipada - White</li>
+          <li className="text-gray-900">Second Day (27th September) - Dwitiya - Red</li>
+          <li className="text-gray-900">Third-Day (28th September) - Tritiya - Royal Blue</li>
+          <li className="text-gray-900">Fourth Day (29th September) - Chaturthi - Yellow</li>
+          <li className="text-gray-900">Fifth Day (30th September) - Panchami - Green</li>
+          <li className="text-gray-900">Sixth Day (1st October) - Shashti - Grey</li>
+          <li className="text-gray-900">Seventh Day (2nd October) - Saptami - Orange</li>
+          <li className="text-gray-900">Eighth Day (3rd October) - Ashtami - Peacock Green</li>
+          <li className="text-gray-900">Ninth Day (4th October) - Navami - Pink</li>
+     
+      </p>
+    </div>
+  );
+};
+
+export default About;
